8dac7e4bccf6e371f52f9fbdb0d7961d3e7cafd2
Omar-MK-20 <omar.mk.201@gmail.com>
2026-02-09 23:17:49 +0200

    Add CORS middleware and integrate invoice router into FastAPI server for improved API functionality

--- Backend/server/main.py
+++ Backend/server/main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.DB.connection import testDBConnection, Base, engine
 import server.Models
 import sys
+
 from server.Routers.customer import customerRouter
 from server.Routers.service import serviceRouter
 from server.Routers.car import carRouter
+from server.Routers.invoice import invoiceRouter
 
 server = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+server.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 server.include_router(customerRouter)
 server.include_router(serviceRouter)
 server.include_router(carRouter)
+server.include_router(invoiceRouter)
 
 if not testDBConnection():
     sys.exit(1)
